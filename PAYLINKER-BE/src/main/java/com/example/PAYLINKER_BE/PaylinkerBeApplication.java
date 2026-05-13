6a93faa6d43020ae83c5e1edeebce4dd9f56f65a
seoyeon <2371013@ewhain.net>
2026-05-13 16:59:52 +0900

    chore: 프로젝트 초기화 및 컨벤션 설정

---
+++ PAYLINKER-BE/src/main/java/com/example/PAYLINKER_BE/PaylinkerBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.PAYLINKER_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaylinkerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaylinkerBeApplication.class, args);
+	}
+
+}
---
+++ PAYLINKER-BE/src/test/java/com/example/PAYLINKER_BE/PaylinkerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PAYLINKER_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaylinkerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
