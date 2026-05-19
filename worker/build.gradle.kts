9f47d1259fcd4984fa8e4f7248c7bcdddbca16d8
Gyeore Kim <h2weirdo@icloud.com>
2026-05-19 23:27:12 +0900

    fix: worker 모듈 bootJar 비활성 및 jar 활성 (#1)

--- worker/build.gradle.kts
+++ worker/build.gradle.kts
@@ -7,3 +7,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+
+tasks.bootJar {
+    enabled = false
+}
+
+tasks.jar {
+    enabled = true
+}
