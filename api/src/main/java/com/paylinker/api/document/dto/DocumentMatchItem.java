7c7b50fa933c1b2d94279fdf982afad66eff96bd
HongYeonLee <llhongyan@naver.com>
2026-05-23 05:03:01 +0900

    refactor: 데이터 타입 Enum에 맞게 수정

--- api/src/main/java/com/paylinker/api/document/dto/DocumentMatchItem.java
+++ api/src/main/java/com/paylinker/api/document/dto/DocumentMatchItem.java
@@ -1,11 +1,13 @@
 package com.paylinker.api.document.dto;
 
+import com.paylinker.api.entity.enums.DocumentMatchStatus;
+
 public record DocumentMatchItem(
         String campaignRecipientId,
         String recipientName,
         String employeeNo,
         String email,
-        String matchStatus,
+        DocumentMatchStatus matchStatus,
         String matchKey,
         String documentId
 ) {
