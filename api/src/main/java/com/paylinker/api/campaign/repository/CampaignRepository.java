df2c5cf3f6eafe096f70c26b31ed48e61fe9aad1
HongYeonLee <llhongyan@naver.com>
2026-05-23 20:47:16 +0900

    feat: 캠페인 목록 조회 Repository 추가 (#23)

---
+++ api/src/main/java/com/paylinker/api/campaign/repository/CampaignRepository.java
@@ -0,0 +1,33 @@
+package com.paylinker.api.repository;
+
+import com.paylinker.api.entity.PaylinkerCampaign;
+import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbIndex;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+
+import java.util.List;
+
+@Repository
+public class CampaignRepository {
+
+    private final DynamoDbTable<PaylinkerCampaign> campaignTable;
+
+    public CampaignRepository(DynamoDbEnhancedClient enhancedClient) {
+        this.campaignTable = enhancedClient.table("paylinker_campaign", TableSchema.fromBean(PaylinkerCampaign.class));
+    }
+
+    public List<PaylinkerCampaign> findAllByAdminId(String adminId) {
+        DynamoDbIndex<PaylinkerCampaign> gsi1 = campaignTable.index(PaylinkerCampaign.INDEX_GSI1);
+
+        // GSI1 PK 생성 규칙인 "ADMIN#{adminId}"를 활용하여 쿼리
+        QueryConditional queryConditional = QueryConditional.keyEqualTo(k ->
+                k.partitionValue(PaylinkerCampaign.gsi1Pk(adminId)));
+
+        return gsi1.query(queryConditional).stream()
+                .flatMap(page -> page.items().stream())
+                .toList();
+    }
+}
