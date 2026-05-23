cef9b740cc11e95e375a46982cd600b5927b1e85
HongYeonLee <llhongyan@naver.com>
2026-05-23 22:15:22 +0900

    feat: 캠페인 생성 Repository 계층 구현 (#37)

--- api/src/main/java/com/paylinker/api/campaign/repository/CampaignRepository.java
+++ api/src/main/java/com/paylinker/api/campaign/repository/CampaignRepository.java
@@ -59,7 +59,7 @@ public class CampaignRepository {
                 .toList();
     }
 
-    // 이름 중복 검사를 위한 메서드 (필터링 최적화 적용)
+    // 이름 중복 검사를 위한 메서드
     public boolean existsByAdminIdAndCampaignName(String adminId, String campaignName) {
         DynamoDbTable<PaylinkerCampaign> campaignTable =
                 enhancedClient.table("paylinker_campaign", TableSchema.fromBean(PaylinkerCampaign.class));
