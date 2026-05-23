efbceaff6d6478b76f26300eade53b34bc7c8004
HongYeonLee <llhongyan@naver.com>
2026-05-23 19:09:09 +0900

    chore: DynamoDB 엔티티 Enum SendFailureReason 추가

---
+++ api/src/main/java/com/paylinker/api/entity/enums/SendFailureReason.java
@@ -0,0 +1,12 @@
+package com.paylinker.api.entity.enums;
+
+public enum SendFailureReason {
+    INVALID_EMAIL,
+    BLOCKED,
+    BOUNCED,
+    COMPLAINT,
+    TEMPORARY_FAILURE,
+    SYSTEM_ERROR,
+    RATE_LIMITED,
+    UNKNOWN
+}
